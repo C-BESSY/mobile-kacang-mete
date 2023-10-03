d61c184894d274faf0ce4d8f3436bb079742fb60
Richie-Z <zakariarichie@yahoo.com>
2023-10-03 15:02:14 +0700

    feat(common): introduce card overview widget

--- lib/common/widget/card_overview_widget.dart
+++ lib/common/widget/card_overview_widget.dart
@@ -10,11 +10,10 @@ class CardOverviewWidget extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     return Column(
       children: [
-        Text(
+        const Text(
           "Tanggal",
           style: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontSize: screenHeight * 0.025,
+            fontWeight: FontWeight.w300,
             color: Colors.black,
           ),
         ),
@@ -26,18 +25,90 @@ class CardOverviewWidget extends StatelessWidget {
             color: Colors.black,
           ),
         ),
-        Container(
-          color: Colors.green.shade500,
-          child: Row(
-            children: [
-              Icon(Icons.add_card),
-              Column(
-                children: [Text('Income'), Text('Rp. 5.000.000')],
-              )
-            ],
-          ),
-        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _CardWidget(
+              mainColor: Colors.green.shade500,
+              title: 'Income',
+              icon: Icons.card_travel,
+              value: "Rp. 5.000.000",
+            ),
+            _CardWidget(
+              mainColor: Colors.red.shade500,
+              title: 'Expense',
+              icon: Icons.card_travel,
+              value: "Rp. 5.000.000",
+            ),
+          ],
+        )
       ],
+    );
+  }
+}
+
+class _CardWidget extends StatelessWidget {
+  final Color mainColor;
+  final String title;
+  final IconData icon;
+  final String value;
+
+  const _CardWidget({
+    required this.mainColor,
+    required this.title,
+    required this.icon,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: screenHeight * 0.025),
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          border: Border.all(
+            color: mainColor,
+            width: 2.0,
+          ),
+          color: mainColor,
+        ),
+        child: Row(
+          children: [
+            const Icon(
+              Icons.add_card,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: screenWidth * 0.025,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenHeight * 0.015,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  value,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenHeight * 0.02,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
