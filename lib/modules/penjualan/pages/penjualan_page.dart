d759d35e411281709ed961016a825728aa93208c
Richie-Z <zakariarichie@yahoo.com>
2023-10-07 18:15:23 +0700

    enhance(penjualan): layout update

--- lib/modules/penjualan/pages/penjualan_page.dart
+++ lib/modules/penjualan/pages/penjualan_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class PenjualanPage extends StatelessWidget {
   const PenjualanPage({super.key});
@@ -9,39 +10,144 @@ class PenjualanPage extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Penjualan")),
-        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        title: Center(
+          child: Transform.translate(
+            offset: Offset(-screenWidth * 0.04, 0),
+            child: Text(
+              "Penjualan",
+              style: TextStyle(
+                fontSize: screenWidth * 0.04,
+              ),
+            ),
+          ),
+        ),
+        backgroundColor: Colors.green.shade300,
       ),
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.green.shade300,
       body: SingleChildScrollView(
-        child: Container(
-          height: screenHeight,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: screenHeight * 0.2,
+          ),
           child: Column(
-            // mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Total'),
-              Text(
-                'Rp. 0',
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: screenWidth * 0.025,
+                  right: screenWidth * 0.025,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Total',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                    Text(
+                      'Rp. 0',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.1,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
               ),
               Container(
+                height: screenHeight * 0.62,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      const BorderRadius.vertical(top: Radius.circular(16)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
                 padding: EdgeInsets.symmetric(
-                    vertical: screenHeight * 0.025,
-                    horizontal: screenWidth * 0.025),
+                  vertical: screenHeight * 0.025,
+                  horizontal: screenWidth * 0.025,
+                ),
                 child: Column(
                   children: [
                     Form(
                       child: Column(
                         children: [
-                          TextFormField(
-                            keyboardType: TextInputType.text,
-                            decoration: InputDecoration(
-                              labelText: "Item",
-                              border: const OutlineInputBorder(),
+                          TypeAheadField(
+                            textFieldConfiguration: TextFieldConfiguration(
+                              autofocus: true,
+                              decoration: InputDecoration(
+                                labelText: "Item",
+                                border: OutlineInputBorder(),
+                                prefixIcon: Icon(Icons.search),
+                              ),
                             ),
-                          )
+                            suggestionsCallback: (pattern) => [
+                              'Kacang Mete',
+                              'Kacang'
+                            ].where((x) => x
+                                .toLowerCase()
+                                .contains(pattern.toLowerCase())),
+                            itemBuilder: (context, suggestion) {
+                              return ListTile(
+                                title: Text(suggestion),
+                              );
+                            },
+                            onSuggestionSelected: (String suggestion) =>
+                                debugPrint(suggestion),
+                          ),
+                          SizedBox(
+                            height: screenHeight * 0.025,
+                          ),
+                          TypeAheadField(
+                            textFieldConfiguration: TextFieldConfiguration(
+                              decoration: InputDecoration(
+                                labelText: "Jenis",
+                                border: OutlineInputBorder(),
+                                prefixIcon: Icon(Icons.search),
+                              ),
+                            ),
+                            suggestionsCallback: (pattern) => [
+                              'Kacang Mete',
+                              'Kacang'
+                            ].where((x) => x
+                                .toLowerCase()
+                                .contains(pattern.toLowerCase())),
+                            itemBuilder: (context, suggestion) {
+                              return ListTile(
+                                title: Text(suggestion),
+                              );
+                            },
+                            onSuggestionSelected: (String suggestion) =>
+                                debugPrint(suggestion),
+                          ),
+                          SizedBox(
+                            height: screenHeight * 0.025,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: "Jumlah",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
                         ],
                       ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => debugPrint("hello"),
+                      child: Text('Center'),
                     ),
                   ],
                 ),
