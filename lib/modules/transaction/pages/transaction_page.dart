03ac8af30b25d44abc3c9c30eadb78f955ce6a43
Chassie <charistyfirst@gmail.com>
2023-10-07 15:32:45 +0700

    feat(page): transaction page introduction

--- lib/modules/transaction/pages/transaction_page.dart
+++ lib/modules/transaction/pages/transaction_page.dart
@@ -3,8 +3,9 @@ import 'package:kacang_mete/common/enums/transaction_type_enum.dart';
 import 'package:kacang_mete/common/widget/card_overview_widget.dart';
 import 'package:kacang_mete/common/widget/transaction_item_widget.dart';
 import 'package:kacang_mete/modules/transaction/widgets/transaction_daily_widget.dart';
+import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
 
-class TransactionPage extends StatefulWidget{
+class TransactionPage extends StatefulWidget {
   const TransactionPage({super.key});
 
   @override
@@ -16,7 +17,6 @@ class _TransactionPageState extends State<TransactionPage> {
 
   @override
   Widget build(BuildContext context) {
-  
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
     return SingleChildScrollView(
@@ -25,13 +25,139 @@ class _TransactionPageState extends State<TransactionPage> {
           Padding(
             padding: EdgeInsets.symmetric(vertical: screenHeight * 0.025),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 TextButton(
                   onPressed: () => debugPrint('should open datepicker'),
-                  child: Row(
+                  child: const Row(
                     children: [
-                      const Icon(Icons.arrow_back),
+                      Icon(Icons.arrow_back),
                       Text("Harian"),
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Colors.white),
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          child: Column(
+                            children: [
+                              const Icon(
+                                Icons.horizontal_rule_rounded,
+                                color: Color.fromARGB(255, 136, 135, 135),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: screenWidth * 0.025),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text(
+                                      'Filter Transaction',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    TextButton(
+                                      onPressed: () =>
+                                          debugPrint('should reset'),
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.grey),
+                                      ),
+                                      child: const Text('Reset'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              ListTile(
+                                title: Text(
+                                  'Sort By',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          debugPrint('Highest');
+                                        },
+                                        child: Text('Highest')),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          debugPrint('Lowest');
+                                        },
+                                        child: Text('Lowest')),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          debugPrint('Newest');
+                                        },
+                                        child: Text('Newest')),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          debugPrint('Oldest');
+                                        },
+                                        child: Text('Oldest')),
+                                  ],
+                                ),
+                                // onTap: () {
+                                //   Navigator.pop(context);
+                                // },
+                              ),
+                              const ListTile(
+                                title: (Text('Date',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))),
+                              ),
+                              ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStatePropertyAll(
+                                        Colors.blue[100]),
+                                    textStyle: MaterialStateProperty.all(
+                                        const TextStyle(color: Colors.black)),
+                                  ),
+                                  onPressed: () =>
+                                      debugPrint('should select date'),
+                                  child: const Text(
+                                    'Select Date',
+                                    style: TextStyle(color: Colors.black),
+                                  )),
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStatePropertyAll(
+                                      const Color.fromARGB(255, 28, 147, 245)),
+                                  textStyle: MaterialStateProperty.all(
+                                      TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 255, 255, 255))),
+                                ),
+                                onPressed: () {
+                                  debugPrint('Should apply filters then close');
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Apply'),
+                              ),
+                              // ListTile(
+                              //   subtitle: Text('Pick Date',textAlign: TextAlign.justify),
+                              //   onTap: () {Navigator.pop(context);},2
+                              // ),
+                              // Text('data'),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(Icons.filter_list),
                     ],
                   ),
                 )
@@ -54,5 +180,4 @@ class _TransactionPageState extends State<TransactionPage> {
       ),
     );
   }
-  
 }
