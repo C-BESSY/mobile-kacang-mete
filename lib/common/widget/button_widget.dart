5d2bff18d607bd8eec9183eaa09ba43351e2fba2
Chassie <charistyfirst@gmail.com>
2023-10-10 21:22:19 +0700

    feat: widget

---
+++ lib/common/widget/button_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final Color color;
+  final VoidCallback onPressed;
+  final Color defaultColor = Color.fromARGB(246, 105, 16, 206);
+  ButtonWidget(this.onPressed, {super.key, title, color})
+      : text = title ?? "Apply",
+      color = color ?? Color.fromARGB(246, 105, 16, 206);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding:
+        EdgeInsets.only(top: color == defaultColor ? screenHeight * 0.1 : 0),
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(vertical: screenHeight * 0.01),
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            fixedSize:
+              MaterialStateProperty.all(Size.fromHeight(screenHeight * 0.06)),
+            foregroundColor: MaterialStateProperty.resolveWith<Color>(
+              (Set<MaterialState> states) {
+                return Colors.white;
+              },
+            ),
+            backgroundColor: MaterialStateProperty.resolveWith<Color>(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.pressed)) {
+                  return color.withOpacity(0.5);
+                } else {
+                  return color;
+                }
+              },
+            ),
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
