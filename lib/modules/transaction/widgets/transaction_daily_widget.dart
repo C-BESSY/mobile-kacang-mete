b2c3dccd015b9c75a2fa1958df93fcb7103bd77b
Chassie <charistyfirst@gmail.com>
2023-10-10 20:58:41 +0700

    Refactor: Text

--- lib/modules/transaction/widgets/transaction_daily_widget.dart
+++ lib/modules/transaction/widgets/transaction_daily_widget.dart
@@ -4,7 +4,7 @@ import 'package:kacang_mete/common/widget/transaction_item_widget.dart';
 
 class TransactionDailyWidget extends StatelessWidget {
   final String selectedMonth;
-  
+
   const TransactionDailyWidget({super.key, required this.selectedMonth});
   @override
   Widget build(BuildContext context) {
@@ -13,46 +13,54 @@ class TransactionDailyWidget extends StatelessWidget {
     return Column(
       children: [
         Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: screenHeight * 0.03,
-              horizontal: screenWidth * 0.025,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
+          padding: EdgeInsets.symmetric(
+            vertical: screenHeight * 0.03,
+            horizontal: screenWidth * 0.025,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
                 "1 $selectedMonth",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w900,
-                    fontSize: screenWidth * 0.035,
-                  ),
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: screenWidth * 0.035,
                 ),
-                Row(
-                  children: [
-                    Text("Rp. 1.000.000", style: TextStyle(color: Colors.green),),
-                    SizedBox(width: screenWidth * 0.025,),
-                    Text("Rp. 1.000.000", style: TextStyle(color: Colors.red),),
-                  ],
-                )
-              ],
-            ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Rp. 1.000.000",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                  SizedBox(
+                    width: screenWidth * 0.025,
+                  ),
+                  Text(
+                    "Rp. 1.000.000",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ],
+              )
+            ],
           ),
-          ListView.separated(
-            shrinkWrap: true,
-            itemCount: 2,
-            separatorBuilder: (context, index) =>
-                SizedBox(height: screenHeight * 0.02),
-            itemBuilder: (context, index) {
-              return TransactionItemWidget(
-                type: index % 2 == 0
-                    ? TransactionType.pengeluaran
-                    : TransactionType.pemasukan,
-                item: "Kacang Mete",
-                ammount: "Rp. 1.000.000",
-                date: "1 Okt 2023",
-              );
-            },
-          ),
+        ),
+        ListView.separated(
+          shrinkWrap: true,
+          itemCount: 2,
+          separatorBuilder: (context, index) =>
+              SizedBox(height: screenHeight * 0.02),
+          itemBuilder: (context, index) {
+            return TransactionItemWidget(
+              type: index % 2 == 0
+                  ? TransactionType.pengeluaran
+                  : TransactionType.penjualan,
+              item: "Kacang Mete",
+              ammount: "Rp. 1.000.000",
+              date: "1 Okt 2023",
+            );
+          },
+        ),
       ],
     );
   }
