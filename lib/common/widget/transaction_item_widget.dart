2b545e38cbd24a632633c989507c028bf143da8f
Chassie <charistyfirst@gmail.com>
2023-10-11 00:58:42 +0700

    refactor: container, image, text

--- lib/common/widget/transaction_item_widget.dart
+++ lib/common/widget/transaction_item_widget.dart
@@ -23,40 +23,57 @@ class TransactionItemWidget extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: screenHeight * 0.04),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Container(
-                  decoration: BoxDecoration(color: Colors.grey.shade300),
-                  child: Icon(isPengeluaran ? Icons.abc : Icons.add_card,
-                      size: screenWidth * 0.10)),
-              SizedBox(width: screenWidth * 0.05),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(type.value, style: TextStyle(fontSize: screenWidth * 0.03, fontWeight: FontWeight.bold),),
-                  Text(item, style: TextStyle(fontWeight: FontWeight.w100, fontSize: 12, color: Colors.grey[600]),),
-                ],
-              ),
-            ],
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                ammount,
-                style: TextStyle(
-                  color: isPengeluaran
-                      ? Colors.red.shade500
-                      : Colors.green.shade500,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.grey.shade100,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Image.asset(isPengeluaran
+                    ? 'assets/icons/pengeluaran.png'
+                    : 'assets/icons/penjualan.png'),
+                SizedBox(width: screenWidth * 0.05),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      type.value,
+                      style: TextStyle(
+                          fontSize: screenWidth * 0.03,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      item,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w100,
+                          fontSize: 12,
+                          color: Colors.grey[600]),
+                    ),
+                  ],
                 ),
-              ),
-              Text(date),
-            ],
-          )
-        ],
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  ammount,
+                  style: TextStyle(
+                    color: isPengeluaran
+                        ? Colors.red.shade500
+                        : Colors.green.shade500,
+                  ),
+                ),
+                Text(date),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
