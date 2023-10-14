ed77d088a161049e9f43c1b2dede0312342ea4da
Richie-Z <zakariarichie@yahoo.com>
2023-10-14 10:27:05 +0700

    feat: introduce features

---
+++ lib/features/pembelian/types/pembelian_type.dart
@@ -0,0 +1,12 @@
+class PembelianType {
+  final int id;
+  final String pembelian;
+  final int harga;
+  final String keterangan;
+  const PembelianType({
+    required this.id,
+    required this.pembelian,
+    required this.harga,
+    required this.keterangan,
+  });
+}
