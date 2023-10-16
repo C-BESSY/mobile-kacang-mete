594c90a7c65d9c73f6482fdcd082022ba65d29c2
Richie-Z <zakariarichie@yahoo.com>
2023-10-16 22:03:49 +0700

    enhance(item-type): add factory to itemvarian; add to map to itemvarian

--- lib/features/item/types/item_varian_type.dart
+++ lib/features/item/types/item_varian_type.dart
@@ -7,4 +7,19 @@ class ItemVarianType {
     required this.varian,
     required this.harga,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'varian': varian,
+      'harga': harga,
+    };
+  }
+
+  factory ItemVarianType.fromDB(Map<String, dynamic> data) {
+    return ItemVarianType(
+      id: data['id'],
+      varian: data['varian'],
+      harga: data['harga'],
+    );
+  }
 }
