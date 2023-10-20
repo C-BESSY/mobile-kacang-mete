8fbe1207cf35914312426cf031932eb386bc1d7d
Chassie <charistyfirst@gmail.com>
2023-10-20 12:59:54 +0700

    enchance: frontEnd weekly Income Expense

--- lib/modules/transaction/widgets/transaction_weekly_widget.dart
+++ lib/modules/transaction/widgets/transaction_weekly_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kacang_mete/common/utils/helper_util.dart';
+import 'package:kacang_mete/modules/transaction/repository/transaction_repository.dart';
+import 'package:kacang_mete/modules/transaction/types/weekly_income_expense_type.dart';
 
 class TransactionWeeklyWidget extends StatefulWidget {
   final DateTime selectedDate;
@@ -23,6 +26,7 @@ class _TransactionWeeklyWidgetState extends State<TransactionWeeklyWidget> {
     }
     return weekStartDates;
   }
+  final TransactionRepository transactionRepository = TransactionRepository();
 
   @override
   Widget build(BuildContext context) {
@@ -30,73 +34,85 @@ class _TransactionWeeklyWidgetState extends State<TransactionWeeklyWidget> {
     final screenWidth = MediaQuery.of(context).size.width;
     final weekStartDates = _calculateWeekStartDates(widget.selectedDate);
     final numberOfWeeks = weekStartDates.length;
-
-    // final numberOfWeeks = 5;
-
+    
     return Column(
       children: [
         // CardOverviewWidget(
-        //   title: "$selectedMonth",
-        //   description: "Weekly Transaction",
+        //   description: "Weekly Transaction", 
+        //   overviewData: OverviewData(
+        //     pembelian: 0, 
+        //     penjualan: 0, 
+        //     balance: 0
+        //   ),
         // ),
         ...List.generate(numberOfWeeks, (weekIndex) {
           final weekNumber = weekIndex + 1;
           final startDate = weekStartDates[weekIndex];
           final endDate = weekIndex < weekStartDates.length - 1
-              ? weekStartDates[weekIndex + 1].subtract(Duration(days: 1))
+              ? weekStartDates[weekIndex + 1].subtract(const Duration(days: 1))
               : DateTime(startDate.year, startDate.month, startDate.day + 6);
 
           final dateRange =
               "${startDate.day} ${_getMonthName(startDate.month)} - ${endDate.day} ${_getMonthName(endDate.month)}";
 
-          return Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: screenHeight * 0.03,
-                  horizontal: screenWidth * 0.025,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Text(
+          return FutureBuilder<WeeklyIncomeExpenseType>(
+            future: transactionRepository.getWeeklyIncomeExpense(startDate, endDate), builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              final incomeExpense = snapshot.data;
+              
+              return Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: screenHeight * 0.03,
+                    horizontal: screenWidth * 0.025,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
                         "Week $weekNumber",
                         style: TextStyle(
                           fontWeight: FontWeight.w900,
                           fontSize: screenWidth * 0.035,
                         ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        const Text(
-                          "Rp. 1.000.000",
-                          style: TextStyle(color: Colors.green),
-                        ),
-                        SizedBox(
-                          width: screenWidth * 0.025,
-                        ),
-                        const Text(
-                          "Rp. 1.000.000",
-                          style: TextStyle(color: Colors.red),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(right: screenWidth * 0.7),
-                child: Text(
-                  dateRange,
-                  style: TextStyle(
-                    fontSize: screenWidth * 0.03,
-                    backgroundColor: Colors.grey.shade300,
+                      Row(
+                        children: [
+                          Text(
+                            intToIDR(incomeExpense!.sumIncome),
+                            style: const TextStyle(color: Colors.green),
+                          ),
+                          SizedBox(
+                            width: screenWidth * 0.025,
+                          ),
+                          Text(
+                            intToIDR(incomeExpense.sumExpense),
+                            style: const TextStyle(color: Colors.red),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  margin: EdgeInsets.only(right: screenWidth * 0.7),
+                  child: Text(
+                    dateRange,
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.03,
+                      backgroundColor: Colors.grey.shade300,
+                    ),
+                  ),
+                ),
+              ],
+              );
+              }
+            }
           );
         }),
       ],
